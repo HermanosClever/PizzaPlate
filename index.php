c0765d93c393cb5a6dc2e939b5b177f719fbbaa0
Oneeyedman <carlos@sidiostedalimones.com>
2014-05-26 18:43:21 +0200

    New and shiner project structure
    - Added a developer folder for raw js, plugins and scss files: /_dev
    - Added a content image follder: /img
    - Added a layout folder: css/layout/v1
    - Some minor adjustments :)
    - Codekit config file updated

--- index.php
+++ index.php
@@ -6,12 +6,11 @@ $lang = 'es';
 <?php include 'incl/_cc.inc'; ?>
 	<head>
 		<meta charset="utf-8" />
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 		<title>&lt;PizzaPlate /&gt;</title>
 		<?php include 'incl/_head.inc'; ?>
 	</head>
-	<body class="<?php echo $page; ?> ">
-		<div class="page_container">
+	<body class="<?php echo $page; ?> _dev">
+		<div class="page">
 
 			<?php include 'incl/_header.inc'; ?>
 
@@ -19,19 +18,10 @@ $lang = 'es';
 			<section id="main">
 				<div class="wrapper">
 					<p>(Main content)</p>
-					<div></div>
 				</div>
 			</section>
 
 			<?php include 'incl/_footer.inc'; ?>
-			<img src="http://placehold.it/300x300"/>
-
-			<ol>
-				<li>Paco 1</li>
-				<li>Paco 2</li>
-				<li>Paco 3</li>
-
-			</ol>
 
 		</div>
 		<?php include 'incl/_js.inc'; ?>
