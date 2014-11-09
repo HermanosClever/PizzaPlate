605bad230cc0876e333d56028d2ef49f9c49ffb8
Carlos Mañas Aldaya <carlos@sidiostedalimones.com>
2014-11-09 18:47:35 +0100

    Issues #3, #6 - Updated LOPD Cookie assets - Reviewed mixins and functions

--- js/main.js
+++ js/main.js
@@ -1,30 +1,30 @@
 var COOKIES = {
 
-	clave : null,
-	codigo : null,
-	contenedor : null,
+	ga_id : null,
+	ga_code : null,
+	ga_container : null,
 	lopd : false,
 
-	inicio : function() {
-		COOKIES.clave = 'UA-1159349-11';
-		COOKIES.codigo = "<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', '" + COOKIES.clave + "' , 'hermanosclever.net');ga('send', 'pageview');</script>";
-		COOKIES.contenedor = $('#aviso-cookies');
+	init : function() {
+		COOKIES.ga_id = 'UA-XXXXXXX-XX';
+		COOKIES.ga_code = "<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', '" + COOKIES.ga_id + "' , 'hermanosclever.net');ga('send', 'pageview');</script>";
+		COOKIES.ga_container = $('#cookie_alert');
 		console.log('COOKIES (Y,N): ' + $.cookie('lopd'));
 		if ($.cookie('lopd')) {
-			COOKIES.aceptar();
+			COOKIES.accepted();
 		} else {
-			COOKIES.contenedor.addClass('mostrado').find('button').on('click', function() {
+			COOKIES.ga_container.addClass('shown').find('button').on('click', function() {
 				if (!COOKIES.lopd) {
-					COOKIES.aceptar();
+					COOKIES.accepted();
 				}
 			});
 		}
 	},
 
-	aceptar : function() {
+	accepted : function() {
 		$.cookie('lopd', 'true');
 		COOKIES.lopd = true;
-		COOKIES.contenedor.removeClass('mostrado').html(COOKIES.codigo);
+		COOKIES.ga_container.removeClass('shown').html(COOKIES.ga_code);
 	}
 
 }
@@ -42,6 +42,6 @@ $(document).ready(function() {
 	// External links
 	$('.legal a, a.ext').attr('target', '_blank');
 
-	COOKIES.inicio();
+	COOKIES.init();
 
 });
